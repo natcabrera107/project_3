6918b9e8b93bbf04859f291c618c3ee2eace1f86
alexanderperkins <alexander.c.perkins@gmail.com>
2026-07-17 12:41:11 -0400

    add frontend build

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,16 @@ import passport from 'passport';
 import './auth/passportConfig.js';
 import authRouter from './routes/auth.js';
 
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(join(__dirname, '../frontend/dist')));
 
 // session setup
 app.use(session({
